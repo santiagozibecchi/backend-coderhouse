7e6981cf9562ca60317c6ab9deb85d62107754ac
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2023-09-17 20:11:32 -0300

    Add more options just for practices

--- clases-EcmaScript/srcAlternative/app.js
+++ clases-EcmaScript/srcAlternative/app.js
@@ -61,67 +61,87 @@ class ProductManager {
 
         return product;
     };
+
+    // Add more options just for practices
+    getTotalProduct(){
+        return this.#products.length
+    }
+
+    getProductsShort(){
+
+        const products = structuredClone(this.#products);
+
+        const orderProducts =  products.sort((a, b) => {
+            if (a.title > b.title) {
+              return 1;
+            }
+            if (a.title < b.title) {
+              return -1;
+            }
+            // a must be equal to b
+            return 0;
+          });
+
+          return orderProducts;
+    };
+
+    modifyProductById(id, renameProduct){
+
+        const { title, price, thumbnail, stock } = renameProduct;
+
+        const product = this.#products.find( prod => prod.id === id);
+        // I modify the object reference! 
+        
+        if (title){
+            product.title = title;
+        };
+        if (price) {
+            product.price = price;
+        };
+        if (thumbnail) {
+            product.thumbnail = thumbnail;
+        };
+        if (stock) {
+            product.stock = stock;
+        };
+
+        return product;
+    };
+
 };
 
 
 const productManager = new ProductManager();
 
-
 const product = buildProduct({getUUID});
-
-
-console.log("1. ", productManager.getProducts());
-
-const product1 = product({
-    title: "Mermelada",
-    code:"MER123",
-    price: 750,
-    stock: 15,
-});
-
-const product2 = product({
-    title: "Fideo Marolio",
-    price: "652",
-    thumbnail: "https://http2.mlstatic.com/D_NQ_NP_638506-MLA48521707549_122021-O.webp",
-    code: "ATR999",
-    stock: 7,
-});
-
-
-const product3 = {
-    id: getUUID(),
-    // title: "Dulce de Leche",
-    // price: "652",
-    code: "DUL258",
-    stock: 3,
-};
+const product1 = product({ title: "Mermelada", code:"MER123", price: 750, stock: 15 });
+const product2 = product({ title: "Fideo Marolio", price: "652", thumbnail: "https://http2.mlstatic.com/D_NQ_NP_638506-MLA48521707549_122021-O.webp", code: "ATR999", stock: 2 });
+const product3 = product({ title: "Dulce de Leche", price: "652", code: "ATR999", stock: 10 });
+const product4 = product({ title: "Arroz", price: "355", code: "ATR999", stock: 70 });
+const product5 = product({ title: "Proteina", price: "4599", code: "ATR999", stock: 8 });
+const product6 = product({ title: "Sopa", price: "700", code: "ATR999", stock: 3 });
 
 
 productManager.addProduct(product1);
-console.log("2. ", productManager.getProducts());
-
-// No agrega un producto que ya se encuentre con el mismo ID:
-productManager.addProduct(product1);
-console.log("3. ", productManager.getProducts());
-
 productManager.addProduct(product2);
-console.log("4. ", productManager.getProducts());
-
-// Agregar un producto sin un campo requerido
 productManager.addProduct(product3);
-console.log("5. ", productManager.getProducts());
+productManager.addProduct(product4);
+productManager.addProduct(product5);
+productManager.addProduct(product6);
 
-console.log("Obtener producto por ID: ", productManager.getProductById(product1));
+console.log("Productos ", productManager.getProducts());
+console.log(productManager.getTotalProduct());
+console.log("Productos Ordenados!", productManager.getProductsShort());
+console.log("Productos DESORDENADOS!", productManager.getProducts());
 
-const productWithInvalidId = {
-    id: 3333,
-    title: "Dulce de Leche",
-    price: "652",
-    code: "ASD234",
-    stock: 3,
-};
+// Modifico la mermelada:
+console.log(productManager.modifyProductById(product1.id, {title: "Mermelada de Durazno"}));
 
-console.log("Obtener producto por ID: ", productManager.getProductById(productWithInvalidId).message);
+// Obtengo todos los productos nuevamente!
+console.log("Obtengo todos los productos nuevamente!", productManager.getProducts());
+
+
+
 
 
 
