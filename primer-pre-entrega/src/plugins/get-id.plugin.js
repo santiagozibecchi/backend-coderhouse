3e59748e08b099bb0f57663edc33ce8c38207917
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2023-10-09 20:44:57 -0300

    refactor POST endpoints in cart

--- primer-pre-entrega/src/plugins/get-id.plugin.js
+++ primer-pre-entrega/src/plugins/get-id.plugin.js
@@ -5,5 +5,5 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const getUUID = () => {
 
-  return uuidv4();
+  return uuidv4().slice(0, 8);
 };
--- primer-pre-entrega/src/routes/carts.route.js
+++ primer-pre-entrega/src/routes/carts.route.js
@@ -1,6 +1,6 @@
 import { Router, json } from "express";
 import { getUUID } from "../plugins/index.js";
-import { saveToFileSystem, getAllFromFileSystem, validateRequireFields, validateTypes } from "../utils/index.js";
+import { saveToFileSystem, getAllFromFileSystem } from "../utils/index.js";
 
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -13,71 +13,65 @@ const router = Router();
 
 
 const cartPath = path.join(__dirname, "../../db/carts/carts.json");
-const allCarts = await getAllFromFileSystem(cartPath);
 
 // POST
-router.post('/:cid/product/:pid', async (req, res) => {
+router.post('/carts', async (req, res) => {
 
-    const { cid, pid } = req.params;
+    const allCarts = await getAllFromFileSystem(cartPath);
 
-    const cartsInDB = allCarts;
-    
-    const isCartInDB = cartsInDB.find(c => c.id === Number(cid));
-
-    if (!isCartInDB){
-        const newCart = {
-            id: Number(cid),
-            products: [
-                {
-                    productId: pid,
-                    quantity: 1,
-                }
-            ]
-        };
-        cartsInDB.push(newCart);
-    };
-
-    cartsInDB.forEach( cart => {
-        if (cart.id === Number(cid)){
-            
-            const isProductInCart =  cart.products.find( p => p.productId === pid);
-
-            cart.products.forEach( prod => {
-
-                if (prod.productId === pid && isCartInDB){
-                    prod.quantity++;
-                };
-
-                if (!isProductInCart) {
-                    const newProduct = {
-                        productId: pid,
-                        quantity: 1,
-                    };
-
-                    cart.products.push(newProduct);
-                };
-
-
-            });
-        };
+    allCarts.push({
+        id: getUUID(),
+        products: []
     });
     
     try {
-        await saveToFileSystem(cartPath, cartsInDB);
+        
+        await saveToFileSystem(cartPath, allCarts)
+
     } catch (error) {
         console.log(error);
-        return res.status(500).json({status: "error", message: "Internal Server Error"})
+    }
+
+});
+
+router.post('/carts/:cid/product/:pid', async (req, res) => {
+    
+    const cartsInDB = await getAllFromFileSystem(cartPath);
+
+    const { cid, pid } = req.params;
+    
+    const cartInDB = cartsInDB.find(c => c.id === cid);
+
+    const newProduct = {
+        productId: pid,
+        quantity: 1
     };
+    
+    if (cartInDB){
+        
+        const product = cartInDB.products.find( product => product.productId === pid);
 
-    const cart = cartsInDB.find( cart => cart.id === Number(cid));
+        product ? product.quantity++ : cartInDB.products.push(newProduct)
 
-    return res.status(201).json({status: "success", payload: cart});
+        try {
+        
+            await saveToFileSystem(cartPath, cartsInDB)
+    
+        } catch (error) {
+            console.log(error);
+        };
+
+
+    } else {
+        return res.status(404).json({ status: "error", message: "Carrito no encontrado" });
+    };
 
 });
 
 
-
 // TODO GET
+
+
 
 
 
